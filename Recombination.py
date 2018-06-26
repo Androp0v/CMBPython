4eb432b9905a70d675bf460059931d47547948f1
Androp0v <raul_m_p@me.com>
2018-06-26 13:18:11 +0200

    Update Recombination.py - v1.0

--- Recombination.py
+++ Recombination.py
@@ -7,7 +7,7 @@ import sys
 #Helper functions:
 
 def H(a):
-	return (H0/(3.085678*10**19)) * sqrt( (omega_b+omega_m)*a**(-3) + omega_r*a**(-4) + omega_lambda ) #Dimensions: ?
+	return (H0/(3.085678*10**19)) * sqrt( (omega_b+omega_m)*a**(-3) + omega_r*a**(-4) + omega_lambda ) #Dimensions: 1/s
 
 def n_b(a):
 	ro_c = 3*( (H0/(3.085678*10**19)) **2) / (8*pi*G)
@@ -21,26 +21,26 @@ def SahaEquation(a):
 	return 0.5 * (-coefficient + sqrt(coefficient**2 + 4*coefficient)) #Dimensionless
 
 def phi2(a):
-	if T_b(a) > 6000:
+	if T_b(a) > 1000000:
 		return 0
 	else:
-		return 0.448*np.log(epsilon_0/kBoltzmann*T_b(a)) #Dimensionless
+		return 0.448*np.log(epsilon_0/(kBoltzmann*T_b(a))) #Dimensionless
 
 def alpha2(a):
-	return 64*pi/sqrt(27*pi) * (alphaFina**2 * h2**2)/(m_e**2 * c) * sqrt(epsilon_0/(kBoltzmann*T_b(a))) * phi2(a)
+	return 64*pi/sqrt(27*pi) * (alphaFina**2 * h2**2)/(m_e**2 * c) * sqrt(epsilon_0/(kBoltzmann*T_b(a))) * phi2(a) #m3/s
 
 def beta(a):
-	return (m_e*kBoltzmannSI*T_b(a) / (2*pi*h2**2))**1.5 * exp(-epsilon_0/(kBoltzmann*T_b(a))) * alpha2(a) #Dimensions: ?
+	return (m_e*kBoltzmannSI*T_b(a) / (2*pi*h2**2))**1.5 * exp(-epsilon_0/(kBoltzmann*T_b(a))) * alpha2(a) #Dimensions: 1/m3
 
 def beta2(a):
-	return beta(a) * exp(3*epsilon_0/(4*kBoltzmann*T_b(a))) #Dimensions: ?
+	return beta(a) * exp(3*epsilon_0/(4*kBoltzmann*T_b(a))) #Dimensions: 1/m3
 
 def n1s(Xe, a):
 	return (1 - Xe)*n_b(a) #1/m3
 
 def LambdaAlpha(Xe, a):
 	#¿Qué constante de Planck usar?
-	return H(a) * ((3*epsilon_0SI)**3) / ((8*pi)**2 * n1s(Xe, a) * h**3 * c**3) #Dimensions: 1/s
+	return H(a) * ((3*epsilon_0SI)**3) / ((8*pi)**2 * n1s(Xe, a) * h2**3 * c**3) #Dimensions: 1/s
 
 def C_r(Xe, a):
 	return (Lambda21 + LambdaAlpha(Xe, a)) / (Lambda21 + LambdaAlpha(Xe, a) + beta2(a)) #Dimensionless
@@ -48,7 +48,7 @@ def C_r(Xe, a):
 def XeDerivative(Xe, a):
 	return C_r(Xe, a)/(a*H(a)) * (beta(a) * (1 - Xe) - n_b(a)*alpha2(a)*Xe**2) #Dimensionless
 
-def rungeKutta4(x, derivX, deltat=0.1, **kwargs):
+def rungeKutta4(x, derivX, t = None, deltat=0.1):
 	"""
 		Runge-Kutta 4th order numerical ODE, FIXED STEPSIZE.
 		- x is a numpy array with all the variables to integrate 
@@ -56,31 +56,35 @@ def rungeKutta4(x, derivX, deltat=0.1, **kwargs):
 		- deltat is the time step to use in the algorithm (defaults to 0.1)
 		- **kwargs parameters of derivX function
 	"""
-	k1 = derivX(x,**kwargs)*deltat
-	k2 = derivX(x + 0.5*k1, **kwargs)*deltat
-	k3 = derivX(x + 0.5*k2, **kwargs)*deltat
-	k4 = derivX(x + k3, **kwargs)*deltat
+	k1 = derivX(x,t)*deltat
+	k2 = derivX(x + 0.5*k1, t + deltat/2)*deltat
+	k3 = derivX(x + 0.5*k2, t + deltat/2)*deltat
+	k4 = derivX(x + k3, t)*deltat
 	return x + (k1+2*k2+2*k3+k4)/6.0
 
 #Main program:
 
-z1 = np.linspace(1800, 1587.4, 100) #Valid Saha interval, 100 points, Xe becomes 0.99 at redshift 1587.4 based on Saha's approximation
-z2 = np.linspace(1587.4, 200, 600) #Valid Peebles interval, 600 points
+#Create two grids for Saha and Peebles redshift intervals:
 
-print("Entering loop: \n")
+z1 = np.linspace(2500, 1587.4, 1200) #Valid Saha interval, 100 points, Xe becomes 0.99 at redshift 1587.4 based on Saha's approximation
+z2 = np.linspace(1587.4, 65, 100000) #Valid Peebles interval, 100000 points
 
-XeGrid = np.empty(600)
-n_bGrid = np.empty(600)
-T_bGrid = np.empty(600)
-phi2Grid = np.empty(600)
-alpha2Grid = np.empty(600)
-betaGrid = np.empty(600)
-beta2Grid = np.empty(600)
-n1sGrid = np.empty(600)
-LambdaAlphaGrid = np.empty(600)
-C_rGrid = np.empty(600)
-XeDerivativeGrid = np.empty(600)
 
+#Create empty arrays to store intermediate variables for plotting Peebles equation parameters:
+
+XeGrid = np.empty(100000)
+n_bGrid = np.empty(100000)
+T_bGrid = np.empty(100000)
+phi2Grid = np.empty(100000)
+alpha2Grid = np.empty(100000)
+betaGrid = np.empty(100000)
+beta2Grid = np.empty(100000)
+n1sGrid = np.empty(100000)
+LambdaAlphaGrid = np.empty(100000)
+C_rGrid = np.empty(100000)
+XeDerivativeGrid = np.empty(100000)
+
+#Initial values for Peebles equation are given from Saha's equation:
 
 XeGrid[0] = SahaEquation(1/(1 + 1587.4))
 n_bGrid[0] = n_b(1/(1 + 1587.4))
@@ -94,10 +98,11 @@ LambdaAlphaGrid[0] = LambdaAlpha(XeGrid[0], 1/(1 + 1587.4))
 C_rGrid[0] = C_r(XeGrid[0], 1/(1 + 1587.4))
 XeDerivativeGrid[0] = XeDerivative(XeGrid[0], 1/(1 + 1587.4))
 
+#Runge-Kutta grid to integrate Xe over the Peebles interval:
 
 for i in range(1,len(z2)):
 	Xe = XeGrid[i-1]
-	Xe = rungeKutta4(Xe, XeDerivative, deltat = (np.absolute(1/(1+z2[i]) - 1/(1+z2[i-1]))), a = 1/(1+z2[i-1])) #Integrate for one redshift to the next in the grid
+	Xe = rungeKutta4(Xe, XeDerivative, t = 1/(1+z2[i-1]), deltat = (np.absolute(1/(1+z2[i]) - 1/(1+z2[i-1]))))
 
 	current_a = 1/(1+z2[i])
 
@@ -114,37 +119,57 @@ for i in range(1,len(z2)):
 	XeDerivativeGrid[i] = XeDerivative(Xe, current_a)
 
 	print("z = " + str(z2[i]), ", Xe = " + str(XeGrid[i]))
-	#print("XeDerivative = " + str(XeDerivative(Xe, current_a)))
 
 
-
-#Plotting:
+#Plotting and saving to .txt:
 
 #First window, plot for Saha and Peebles' equations vs redshift
 
 fig1 = plt.figure()
 
-b = np.empty(100+600)
+b = np.zeros(1200+100000)
 
 for i in range(len(z1)):
 	b[i] = SahaEquation(1/(1+z1[i]))
 
 for i in range(len(z2)):
-	b[i+100] = SahaEquation(1/(1+z2[i]))
+	b[i+1200] = SahaEquation(1/(1+z2[i]))
 
-plt.semilogy(z1, b[:100], color='b')
+plt.semilogy(z1, b[:1200], color='b')
+##plt.plot(z1, b[:100], color='b')
 plt.semilogy(z2, XeGrid, color = 'b')
-plt.semilogy(z2, b[100:700], linestyle='--', color='b', linewidth=1)
+plt.semilogy(z2, b[1200:(1200+100000)], linestyle='--', color='b', linewidth=1)
+##plt.plot(z2, b[100:(100+100000)], linestyle='--', color='b', linewidth=1)
 plt.xlim(np.max(z1), np.min(z2))
+##plt.xlim(np.max(z1), 1550)
 #plt.ylim(np.min(XeGrid), 10)
 plt.ylim(10**(-4), 1.5)
+##plt.ylim(0.98, 1.001)
 plt.axvline(1587.4, color = 'red', linewidth=1)
 plt.ylabel("Xe")
 plt.xlabel("z")
 
-#Second window, plot for everything in Peebles' equation:
+#Save to txt:
+
+np.savetxt("XeGrid.txt", np.concatenate((z1,z2)))
+np.savetxt("XeResults.txt", np.concatenate((b[:1200], XeGrid)))
+
+#Second window, non-logarithmic scale:
 
 fig2 = plt.figure()
+
+plt.plot(z1, b[:1200], color='b')
+plt.plot(z2, XeGrid, color = 'b')
+plt.plot(z2, b[1200:(1200+100000)], linestyle='--', color='b', linewidth=1)
+plt.xlim(np.max(z1), np.min(z2))
+plt.ylim(0, 1.1)
+plt.axvline(1587.4, color = 'red', linewidth=1)
+plt.ylabel("Xe")
+plt.xlabel("z")
+
+#Third window, plot for everything in Peebles' equation:
+
+fig3 = plt.figure()
 
 plt.subplot(251)
 plt.title("n_b")
@@ -196,5 +221,24 @@ plt.subplot(2,5,10)
 plt.title("XeDerivative")
 plt.plot(z2, XeDerivativeGrid)
 plt.xlim(np.max(z2), np.min(z2))
+
+#####
+
+#plt.figure()
+
+# plt.rc('text', usetex=True)
+# plt.title(r"$\Lambda_\alpha$, $\Lambda_{2\rightarrow1}$")
+#plt.plot(z2, LambdaAlphaGrid)
+# plt.axhline(Lambda21, color = 'r')
+# plt.xlim(np.max(z2), np.min(z2))
+# plt.xlabel(r"Redshift $z$")
+# plt.ylabel(r"Velocidad de la reaccion $s^{-1}$")
+
+#plt.rc('text', usetex=True)
+#plt.title(r'Densidad de H en estado fundamental $(n_{1s})$')
+#plt.plot(z2, n1sGrid)
+#plt.xlim(np.max(z2), np.min(z2))
+#plt.xlabel("Redshift (z)")
+#plt.ylabel("Densidad $m^{-3}$")
 
 plt.show()
